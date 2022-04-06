19345af4cb767402761922641d441e651de39007
Diego Bueno <diegobuenomendonca@hotmail.com>
2022-04-06 16:24:44 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
+// import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <SolarSystem>
-          <Title headline="dadadaaddad" />
-        </SolarSystem>
+        <SolarSystem />
       </div>);
   }
 }
