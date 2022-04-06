9170886db0be5ee39c3fd682fc843f103a94dc04
Diego Bueno <diegobuenomendonca@hotmail.com>
2022-04-06 17:54:51 -0300

    Tudo testado - Projeto Finalizado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,25 +1,24 @@
 import React from 'react';
-// import Title from './Title';
-// import missions from '../data/missions';
-// import MissionCard from './MissionCard';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
-    // return (
-    //   <div data-testid="missions">
-    //     <Title headline="Missões" />
-    //     <div>
-    //       {missions.map((mission, index) => (<MissionCard
-    //         name={ mission.name }
-    //         year={ mission.year }
-    //         country={ mission.country }
-    //         destination={ mission.destination }
-    //         key={ index }
-    //       />))}
-    //     </div>
-    //   </div>
-    // );
-    return <div />;
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        <div>
+          {missions.map((mission, index) => (<MissionCard
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+            key={ index }
+          />))}
+        </div>
+      </div>
+    );
   }
 }
 
