3f6e4d422bfb033e2771960cb5781ed578fe069c
Diego Bueno Mendonça <90729625+Diego-Bueno-M@users.noreply.github.com>
2023-03-06 19:23:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
 
-const Div = styled.div`
-  color: white;
-  background-image: url('https://i1.wp.com/multarte.com.br/wp-content/uploads/2016/03/realistic-stars-twinkle-in-the-night-sky-loopable_vyotq2pj__f0000.png?w=1392&ssl=1');
-`;
-
 class App extends React.Component {
   render() {
     return (
-      <Div>
+      <div>
         <Header />
         <SolarSystem />
         <Missions />
-      </Div>
+      </div>
     );
   }
 }
